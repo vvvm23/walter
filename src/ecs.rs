14e1a7e66d718dfb4c867fcd2789f45f88b05775
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2020-05-27 18:17:36 +0100

    Renderable Primitive Component. not integrate yet (walter)

--- src/ecs.rs
+++ src/ecs.rs
@@ -20,18 +20,44 @@ impl RenderableComponent {
     }
 }
 
-enum Shape {
-    Circle,
-    Rectangle,
+pub enum Shape {
+    Circle{ r: f32 }, // Radius
+    Rectangle{ w: f32, h: f32 }, // width, height
 }
 
 pub struct RenderablePrimitiveComponent {
     shape: Shape,
-
+    draw_mode: graphics::DrawMode,
+    colour: graphics::Color,
 }
 
 impl RenderablePrimitiveComponent {
+    pub fn new(shape: Shape, draw_mode: graphics::DrawMode, colour: graphics::Color) -> RenderablePrimitiveComponent {
+        RenderablePrimitiveComponent {
+            shape: shape,
+            draw_mode: draw_mode,
+            colour: colour,
+        }
+    }
 
+    pub fn build_mesh(&self, ctx: &mut Context) -> graphics::Mesh {
+        match self.shape {
+            Shape::Circle{r} => graphics::Mesh::new_circle(
+                ctx,
+                self.draw_mode,
+                na::Point2::new(0.0, 0.0),
+                r,
+                1.0,
+                self.colour,
+            ),
+            Shape::Rectangle{w,h} => graphics::Mesh::new_rectangle(
+                ctx,
+                self.draw_mode,
+                graphics::Rect {x: 0.0, y: 0.0, w: w, h: h},
+                self.colour
+            )
+        }.unwrap()
+    }
 }
 
 pub struct RenderableSpriteComponent {
