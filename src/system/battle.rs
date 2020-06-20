6be818d8730223be233cbbd7780ac15dd2ab5682
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2020-06-20 17:29:04 +0100

    battlestate for locking w/ multithreading (walter)

--- src/system/battle.rs
+++ src/system/battle.rs
@@ -7,7 +7,8 @@ use std::sync::{Arc, RwLock};
 // 1. Get next entity to move
 // 2. Select a move and target(s) in battle thread
 // 3. Push Move Action to action queue
-// 4. In main thread perform animation, audio for move
+// 4. In main thread perform animation, audio for move (main launches another thread to execute
+//    animation)
 // 5. In battle thread apply effects of move
 // 6. If something else happens, push this action as a result
 // 7. In main thread handle sound and animation for resulting action
@@ -20,6 +21,19 @@ use std::sync::{Arc, RwLock};
 // We send requests for audio to be played to main thread and it will execute.
 // Not sure how to synchronise between threads, just waiting is unstable
 
+pub enum Action {
+    Move(Arc<Entity>, Arc<battle::Move>, Arc<Entity>),
+    Down(Arc<Entity>),
+}
+
+// not sure if needed yet, may just be able to infer 
+pub enum BattleState {
+    Started,
+    Available,
+    WaitingPlayer,
+    WaitingEvent,
+}
+
 pub struct BattleInstance {
     pub enemy_name: String,
     pub win_message: Option<String>,
@@ -29,6 +43,8 @@ pub struct BattleInstance {
     // music: Arc<ggez::sound::SoundData>
 
     pub entities: Vec<Arc<Entity>>,
+    pub actions: Vec<Action>,
+    pub state: BattleState,
 }
 
 impl BattleInstance {
@@ -38,11 +54,17 @@ impl BattleInstance {
             win_message: None,
             loss_message: None,
             entities: Vec::new(),
+            actions: Vec::new(),
+            state: BattleState::Started,
         }
     }
 
     pub fn add_entities(&mut self, es: &mut Vec<Arc<Entity>>) {
         self.entities.append(es);
+    }
+
+    pub fn add_action(&mut self, action: Action) {
+        self.actions.push(action);
     }
 } 
 
