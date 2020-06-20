ecfb57a18d1fc7c87f4364876f217f03827fa386
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2020-06-20 16:20:12 +0100

    struct for an instance of a battle

--- src/system/battle.rs
+++ src/system/battle.rs
@@ -1,2 +1,30 @@
 use crate::component::battle;
-use std::sync{Arc, RwLock};
+use crate::ecs::{Entity};
+use std::sync::{Arc, RwLock};
+
+pub struct BattleInstance {
+    pub enemy_name: String,
+    pub win_message: Option<String>,
+    pub loss_message: Option<String>,
+
+    // background: Arc<ggez::graphics::Image>,
+    // music: Arc<ggez::sound::SoundData>
+
+    pub entities: Vec<Arc<Entity>>,
+}
+
+impl BattleInstance {
+    pub fn new(enemy_name: &str) -> BattleInstance {
+        BattleInstance {
+            enemy_name: enemy_name.to_string(),
+            win_message: None,
+            loss_message: None,
+            entities: Vec::new(),
+        }
+    }
+
+    pub fn add_entities(&mut self, es: &mut Vec<Arc<Entity>>) {
+        self.entities.append(es);
+    }
+} 
+
--- src/system/mod.rs
+++ src/system/mod.rs
@@ -1,2 +1,3 @@
 pub mod physics;
 pub mod rendering;
+pub mod battle;
