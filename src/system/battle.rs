32d43699a34441a0ece87ae2a21e8aebbcf35718
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2020-06-20 16:51:30 +0100

    a plan of attack on threading between battle and main game loop (walter)

--- src/system/battle.rs
+++ src/system/battle.rs
@@ -2,6 +2,24 @@ use crate::component::battle;
 use crate::ecs::{Entity};
 use std::sync::{Arc, RwLock};
 
+// Battle system plan
+//
+// 1. Get next entity to move
+// 2. Select a move and target(s) in battle thread
+// 3. Push Move Action to action queue
+// 4. In main thread perform animation, audio for move
+// 5. In battle thread apply effects of move
+// 6. If something else happens, push this action as a result
+// 7. In main thread handle sound and animation for resulting action
+// 8. Rotate entity vector
+
+// Alternate strategy
+//
+// All battle logic in another thread, including animation
+// Rendering still occurs in main thread
+// We send requests for audio to be played to main thread and it will execute.
+// Not sure how to synchronise between threads, just waiting is unstable
+
 pub struct BattleInstance {
     pub enemy_name: String,
     pub win_message: Option<String>,
