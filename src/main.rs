7f4899138f7f67e0d6d0db5719b156f57d622c2b
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2020-08-05 13:21:40 +0100

    testing ecs system

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,14 @@ fn main() {
     println!("Aloha World!");
     
     info!("This is an informative message.");
+    
+    let mut x = ecs::State::new();
+    let e1 = x.entity_allocator.allocate();
+    x.null_components.set(e1, ecs::NullComponent{owner: e1.clone()});
+    x.position_components.set(e1, ecs::PositionComponent{owner: e1.clone(), x: 0.0, y: 0.0});
 
-    let mut x = ecs::GenerationalIndexAllocator::new(256);
+    println!("{:?}", x.null_components.get(e1));
+    println!("{:?}", x.position_components.get(e1));
 
-    loop {
-        let i1 = x.allocate();
-        println!("{:?}", i1);
-        x.deallocate(i1);
-    }
-
+    println!("{:?}", x.null_components);
 }
